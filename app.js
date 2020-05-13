df2aea9d827cec3575398fa37d1ce5dc9393f24d
Robert Dirken <robert.dirken@gmail.com>
2020-05-13 23:57:15 +1000

    Initial Commit, File structure created, mobile landing begun

---
+++ app.js
@@ -0,0 +1,21 @@
+const navSlide = () => {
+    const toggle = document.querySelector('#toggle');
+    const nav = document.querySelector('.nav');
+
+    toggle.addEventListener('click',()=>{
+        nav.classList.toggle('open');
+    });
+}
+
+navSlide()
+
+const footerSlide = () => {
+    const icon = document.querySelector('#info-icon');
+    const bar = document.querySelector('.info-bar');
+
+    icon.addEventListener('click',()=>{
+        bar.classList.toggle('info-open');
+    });
+}
+
+footerSlide()
