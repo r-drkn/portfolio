3fa1c490c81d7f48b2e685b48f2db8c05547bfb0
Robert Dirken <robert.dirken@gmail.com>
2020-05-23 11:50:41 +1000

    Mobile Blog debug

--- src/app.js
+++ src/app.js
@@ -2,17 +2,20 @@
 const navSlide = () => {
   const menu = document.querySelector("#menu");
   const nav = document.querySelector(".nav-items");
+  const body = document.querySelector(".grid-container")
   let clicks = 0
   
   menu.addEventListener("click", () => {
     nav.classList.toggle("open");
     if (clicks === 0){
+      body.classList.toggle('noscroll')
       menu.classList.toggle('menu-flip'); 
       const open = () => {(menu.src = 'https://rdrkn-portfolio-bucket.s3-ap-southeast-2.amazonaws.com/close.svg')};
       setTimeout(open, 450);
       clicks++;
     } else {
       menu.classList.toggle("menu-flip");
+      body.classList.toggle('noscroll')
       const close = () => {(menu.src = 'https://rdrkn-portfolio-bucket.s3-ap-southeast-2.amazonaws.com/MENU.svg')};
       setTimeout(close, 250);
       clicks--;
